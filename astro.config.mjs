13de94acde2a6b0ab5e088411d655802ffaffdca
srihari-m-s <srihari.magadi@gmail.com>
2025-12-07 22:54:50 +0530

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,25 @@ import tailwindcss from "@tailwindcss/vite";
 
 import cloudflare from "@astrojs/cloudflare";
 
+const IS_DEVELOPMENT = import.meta.env.MODE === "development";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
   vite: {
+    resolve: {
+      alias: !IS_DEVELOPMENT && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
     plugins: [tailwindcss()],
   },
-  adapter: cloudflare({ imageService: "compile" }),
+  adapter: cloudflare({
+    imageService: "compile",
+    platformProxy: {
+      enabled: true,
+      configPath: "wrangler.jsonc",
+      experimentalJsonConfig: true,
+    },
+  }),
 });
