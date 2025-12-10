63c5fa5afe255397e1a91be71b9b971a09d52e2d
srihari-m-s <srihari.magadi@gmail.com>
2025-12-10 23:00:09 +0530

    update package.json

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -5,7 +5,7 @@ export default new Map([
 ["src/content/authors/lois.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fauthors%2Flois.mdoc&astroContentModuleFlag=true")],
 ["src/content/navbar/index.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fnavbar%2Findex.mdoc&astroContentModuleFlag=true")],
 ["src/content/posts/first-post/index.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ffirst-post%2Findex.mdoc&astroContentModuleFlag=true")],
-["src/content/index-page/index.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Findex-page%2Findex.mdoc&astroContentModuleFlag=true")],
 ["src/content/posts/second-post/index.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fsecond-post%2Findex.mdoc&astroContentModuleFlag=true")],
-["src/content/footer/index.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Ffooter%2Findex.mdoc&astroContentModuleFlag=true")]]);
+["src/content/footer/index.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Ffooter%2Findex.mdoc&astroContentModuleFlag=true")],
+["src/content/index-page/index.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Findex-page%2Findex.mdoc&astroContentModuleFlag=true")]]);
 		
