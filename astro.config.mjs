0ecd5b2551f552a3cd1761c8a4ad6b7f504c2d16
srihari-m-s <srihari.magadi@gmail.com>
2025-12-10 23:24:14 +0530

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 import cloudflare from "@astrojs/cloudflare";
 
+console.log("Current MODE:", import.meta.env.MODE);
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
