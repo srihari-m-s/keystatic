254244e1613c4aa8247a1515ef69d810ce4eb1d0
srihari-m-s <srihari.magadi@gmail.com>
2025-12-10 23:13:24 +0530

    update keystatic config, pkg json

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -21,7 +21,7 @@ const githubStorage: GitHubConfig["storage"] = {
 };
 
 const storage =
-  process.env.NODE_ENV === "development" ? localStorage : githubStorage;
+  import.meta.env.MODE === "development" ? localStorage : githubStorage;
 
 export default config({
   storage: storage,
